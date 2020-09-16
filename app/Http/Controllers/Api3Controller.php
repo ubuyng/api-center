50c0f15a7dd4fa0ead58eecb5f682c3e09184d5b
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-09-16 13:34:22 +0100

    now we revert after test phase 3 was successful

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -177,62 +177,57 @@ class Api3Controller extends Controller
            $des = $request->des;
            $budget = $request->budget;
            $cat_id = $request->cat_id;
-              $sub_id = $request->sub_id;
     
-           $project_data = [
-            'user_id' => $user_id,
-            'project_title' => $task_title,
-            'des' => $des,
-            'budget' => $budget,
-            'cat_id' => $cat_id,
-            'sub_id' => $sub_id,
-        ];
-        // //    get sub category data
-        // $subcat = SubCategory::where('id', $sub_id)->first();
+           $sub_id = $request->sub_id;
+        //    get sub category data
+        $subcat = SubCategory::where('id', $sub_id)->first();
 
-        //    $user_name = $user->first_name." ".$user->last_name;
-        //    $user_number = $user->number;
-        //    $pay_type = $subcat->payment_type;
+           $user_name = $user->first_name." ".$user->last_name;
+           $user_number = $user->number;
+           $pay_type = $subcat->payment_type;
 
-        
-
-        //     $has_q_draft = NewProject::where('user_id', $user_id)->where('status', 0)->first();
+            $has_q_draft = NewProject::where('user_id', $user_id)->where('status', 0)->first();
             
-        //    if($has_q_draft){
-        //     $project_data = [
-        //         'user_id' => $user_id,
-        //         'project_title' => $task_title,
-        //         'sub_category_id' => $sub_id,
-        //         'phone_number' => $user_number,
-        //         'cus_name' => $user_name,
-        //         'project_message' => $des,
-        //         'upay_type' => $pay_type,
-        //     ];
+           if($has_q_draft){
+            $project_data = [
+                'user_id' => $user_id,
+                'project_title' => $task_title,
+                'sub_category_id' => $sub_id,
+                'phone_number' => $user_number,
+                'cus_name' => $user_name,
+                'project_message' => $des,
+                'upay_type' => $pay_type,
+            ];
 
             
-        //     NewProject::where('user_id', $user_id)->where('status', 0)->update($project_data);
-        //    }else{
+            NewProject::where('user_id', $user_id)->where('status', 0)->update($project_data);
+           }else{
 
-        //     $project_data = [
-        //         'user_id' => $user_id,
-        //         'project_title' => $task_title,
-        //         'sub_category_id' => $sub_id,
-        //         'phone_number' => $user_number,
-        //         'cus_name' => $user_name,
-        //         'project_message' => $des,
-        //         'upay_type' => $pay_type,
-        //     ];
+            $project_data = [
+                'user_id' => $user_id,
+                'project_title' => $task_title,
+                'sub_category_id' => $sub_id,
+                'phone_number' => $user_number,
+                'cus_name' => $user_name,
+                'project_message' => $des,
+                'upay_type' => $pay_type,
+            ];
 
-        //     $project_response_data = NewProject::create($project_data);
+            $project_response_data = NewProject::create($project_data);
 
-        //     $project_id = $project_response_data->id;
+            $project_id = $project_response_data->id;
 
-        //    }
-
-       
+           }
 
 
-           $set['UBUYAPI_V2'][]=$project_data;
+           $set['UBUYAPI_V2'][]=array(
+            'user_id' =>$user_id,
+            'project_id' =>$project_id,
+            'des' =>$des,
+            'budget' =>$budget,
+            'cat_id' =>$cat_id,
+            'sub_id' =>$sub_id,
+            'success'=>'1');
 
                 header( 'Content-Type: application/json; charset=utf-8' );
                 echo $val= str_replace('\\/', '/', json_encode($set,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
