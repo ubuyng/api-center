8cb28ad14942badbb5e7fa1af0a36b2096c9ea3b
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-09-16 13:13:54 +0100

    testing 2

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -178,15 +178,25 @@ class Api3Controller extends Controller
            $budget = $request->budget;
            $cat_id = $request->cat_id;
     
-           $sub_id = $request->sub_id;
-        //    get sub category data
-        $subcat = SubCategory::where('id', $sub_id)->first();
+           $project_data = [
+            'user_id' => $user_id,
+            'project_title' => $task_title,
+            'sub_category_id' => $sub_id,
+            'budget' => $budget,
+            'cat_id' => $cat_id,
+            'sub_id' => $sub_id,
+        ];
+        //    $sub_id = $request->sub_id;
+        // //    get sub category data
+        // $subcat = SubCategory::where('id', $sub_id)->first();
 
-           $user_name = $user->first_name." ".$user->last_name;
-           $user_number = $user->number;
-           $pay_type = $subcat->payment_type;
+        //    $user_name = $user->first_name." ".$user->last_name;
+        //    $user_number = $user->number;
+        //    $pay_type = $subcat->payment_type;
 
-            $has_q_draft = NewProject::where('user_id', $user_id)->where('status', 0)->first();
+        
+
+        //     $has_q_draft = NewProject::where('user_id', $user_id)->where('status', 0)->first();
             
         //    if($has_q_draft){
         //     $project_data = [
@@ -219,15 +229,7 @@ class Api3Controller extends Controller
 
         //    }
 
-         $project_data = [
-                'user_id' => $user_id,
-                'project_title' => $task_title,
-                'sub_category_id' => $sub_id,
-                'phone_number' => $user_number,
-                'cus_name' => $user_name,
-                'project_message' => $des,
-                'upay_type' => $pay_type,
-            ];
+       
 
 
            $set['UBUYAPI_V2'][]=$project_data;
