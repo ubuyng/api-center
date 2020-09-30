954a515cf3bb75fd6444168bb267ed9becc8bf21
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-09-30 11:02:53 +0100

    removed select frm v3_project table for inprogress

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -802,7 +802,6 @@ class Api3Controller extends Controller
             dd($v3_projects = DB::table("new_projects") 
                 ->where('new_projects.user_id', '=', $user_id)
                 ->where('new_projects.status', '=', 2)
-                ->select('new_projects.id as project_id', 'new_projects.user_id as user_id',  'new_projects.project_message', 'new_projects.created_at', 'new_projects.sub_category_name','new_projects.status', 'new_projects.sub_category_id','new_projects.address', 'new_projects.budget', 'new_projects.project_title')
                 ->orderBy('new_projects.id', 'desc')->get());
                 
                 /* 
