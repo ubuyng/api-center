e4d98b415935bd43168bbb8019c4bfad208bb243
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-10-27 13:12:42 +0100

    test

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -1703,12 +1703,13 @@ class Api3Controller extends Controller
                 foreach($subs as $sub){
                     $pros = DB::table("ratings")
                     ->join('services', 'services.user_id', '=', 'ratings.pro_id')
+                    ->first()
                     ->where('services.sub_category_id', '=', $sub->id)
                     ->join('users', 'users.id', '=', 'ratings.pro_id')
                     ->select('users.id as id', 'users.image', 'users.first_name', 'users.last_name')
                     ->orderBy('users.id', 'desc')->get();
             
-                    dd($pros);
+                    
                                 foreach ($pros as $pro) {
                                     // getting the pro user details
                                    
