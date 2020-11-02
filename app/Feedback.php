cc1abd2a8110b77a80bb84ac55bc19b5467309e7
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-11-02 15:18:58 +0100

    dd

--- app/Feedback.php
+++ app/Feedback.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    protected $guarded = [];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
    
 }
