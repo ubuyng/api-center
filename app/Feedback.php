2036ff7ba32caa3e31e709be9e972b6a50d8443d
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-11-02 15:21:13 +0100

    dd

--- app/Feedback.php
+++ app/Feedback.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
+    public $fillable = [
+        'user_id',
+        'rating',
+        'feedback',
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
+
     protected $dates = [
         'created_at',
         'updated_at',
