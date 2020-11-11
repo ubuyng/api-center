e845d15c52cea27a2a4cb3ea65af9c088c1b23be
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-11-11 15:49:18 +0100

    dd trending

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -2940,7 +2940,7 @@ public function CallAlertProjectSafety()
                     ->select('profiles.user_id as id', 'profiles.business_name', 'profiles.profile_photo')
                     ->orderBy('profiles.id', 'desc')->get()->take(7);
 
-                 
+                 dd($trending_pros);
                     foreach ($trending_pros as $tpro) {
                         // getting the pro user details
                         $user = User::where('id', $tpro->id)->first();
