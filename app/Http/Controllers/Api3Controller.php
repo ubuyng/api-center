67eaa395d5614245c43fd11c22ea4e48f5930e9e
basicubuy <50665564+basicubuy@users.noreply.github.com>
2020-12-03 11:31:55 +0100

    row fixes test 2

--- app/Http/Controllers/Api3Controller.php
+++ app/Http/Controllers/Api3Controller.php
@@ -1056,8 +1056,8 @@ class Api3Controller extends Controller
          } 
 
 
+         $set['UBUYAPI_V2'] = $row;
             }
-            $set['UBUYAPI_V2'] = $row;
             
             header( 'Content-Type: application/json; charset=utf-8' );
             echo $val= str_replace('\\/', '/', json_encode($set,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
